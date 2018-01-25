c021682ed294cd30afb839580c78736080e4e15d
christopher grant <christopher.grant@csu.fullerton.edu>
2018-01-25 08:07:57 -0800

    whoops

--- new_system.sh
+++ new_system.sh
@@ -8,7 +8,7 @@ rm ~/.bashrc ~/.bash_profile ~/.profile
 stow -t ~ bash
 source ~/.bashrc
 stow -t ~ tmux 
-stow -t ~/.config/nvim/ neovim
+stow -t ~/.config/nvim/ nvim
 stow -t ~ zathura
 mkdir ~/Templates
 stow -t ~/Templates/ templates
